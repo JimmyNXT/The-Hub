a8ac56039e468348c4558d88ab194ec6cb10fb96
Dean Minnie <dminnie33@gmail.com>
2020-10-31 18:24:10 +0200

    First things first

---
+++ main.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 3000;
+
+
+const socketIO = require('socket.io').listen(PORT);
+
+
+socketIO.on('connection', (sock) => 
+{
+    console.log(sock.handshake);
+    sock.emit('message', 'text', 'This is a test');
+
+});
