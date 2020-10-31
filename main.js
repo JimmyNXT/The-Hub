2d30c8d21b5c73a1ef1343531e426777a322a6ca
Dean Minnie <dminnie33@gmail.com>
2020-10-31 20:39:31 +0200

    Broadcast functionality

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ const PORT = process.env.PORT || 3000;
 
 const socketIO = require('socket.io').listen(PORT);
 
+let allTheSockets = [];
+
 
 socketIO.on('connection', (sock) => 
 {
-    //console.log(sock.handshake);
-    sock.emit('message', 'text', 'This is a test');
+    allTheSockets.push(sock);
+    console.log(sock.handshake);
+    sock.on('Broadcast', (type,message) => {
+        allTheSockets.forEach(s => {
+            if(s.disconnected)
+            {
+                allTheSockets.splice(allTheSockets.indexOf(s)+1,1);
+            }
+            else
+            {
+                s.emit('Broadcast',type, message);
+            }
+        });
+    });
 });
-
-socketIO.on('test', (text) => {console.log(test)});
