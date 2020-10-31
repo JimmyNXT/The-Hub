d9475666e35736bb6c2fb2bd96f1c9bff2baacf9
Dean Minnie <dminnie33@gmail.com>
2020-10-31 18:38:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const socketIO = require('socket.io').listen(PORT);
 
 socketIO.on('connection', (sock) => 
 {
-    console.log(sock.handshake);
+    //console.log(sock.handshake);
     sock.emit('message', 'text', 'This is a test');
 });
 
