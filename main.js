4331f3bf6acddb5de1134fda357e514dbe9d014e
Dean Minnie <dminnie33@gmail.com>
2020-10-31 18:47:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ socketIO.on('connection', (sock) =>
 {
     //console.log(sock.handshake);
     sock.emit('message', 'text', 'This is a test');
-    console.log('welp');
 });
 
-socketIO.on('test', (text) => {console.log('The test')});
+socketIO.on('test', (text) => {console.log(test)});
