215b2c8e745de4b4edab92a45fcc9b485526c9f1
Dean Minnie <dminnie33@gmail.com>
2020-10-31 21:38:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const PORT = process.env.PORT || 3000;
 
 
-const socketIO = require('socket.io').listen(PORT);
+const io = require('socket.io').listen(PORT);
 
-let allTheSockets = [];
+/*let allTheSockets = [];
 
 
 socketIO.on('connection', (sock) => 
 {
     allTheSockets.push(sock);
     console.log(sock.handshake);
+
     sock.on('Broadcast', (type,message) => {
         allTheSockets.forEach(s => {
             if(s.disconnected)
@@ -21,5 +22,29 @@ socketIO.on('connection', (sock) =>
                 s.emit('Broadcast',type, message);
             }
         });
+    });
+
+    sock.on('Heartbeat', (type, message) => {
+        console.log(message);
+    });
+});*/
+
+const BROWSER_CLIENTS = {};
+const SERVER_CLIENTS = {};
+
+io.on("connection", socket => {
+    socket.on("source", payload => {
+        if (payload == "browser")
+            BROWSER_CLIENTS[socket.id] = socket;
+        else if (payload == "server")
+            SERVER_CLIENTS[socket.id] = socket;
+    });
+    socket.on("disconnect", () => {
+        delete BROWSER_CLIENTS[socket.id];
+        delete SERVER_CLIENTS[socket.id];
+    });
+
+    sock.on('Heartbeat', (type, message) => {
+        console.log(message);
     });
 });
